ff39fcdad580b1557e2c68f151aa25649fb05fbc
= <mehassan3454@gmail.com>
2025-02-06 18:32:51 +0500

    1.1.0

--- README.md
+++ README.md
@@ -5,15 +5,16 @@ A simple command-line to-do application built with Node.js. This is my first-eve
 ## Features
 - Add tasks
 - View saved tasks
+- Delete saved tasks when completed
 - Simple and easy-to-use CLI interface
 
 ## Screenshot
-![CLI To-Do Application](./screenshot/Screenshot_20250206_105815.png)
-
+![CLI To-Do Application](./screenshot/Screenshot_20250206_182356.png)
+![CLI To-Do Application](./screenshot/Screenshot_20250206_182558.png)
 ## Installation
 1. Clone the repository:
    ```sh
-   git clone https://github.com/Xranus/cli-todo.git
+   git clone https://github.com/Xranus/todo-cli
    cd cli-todo
    ```
 2. Install dependencies:
@@ -28,8 +29,6 @@ node index.js
 ```
 Follow the on-screen prompts to add and view tasks.
 
-## License
-This project is open-source and available under the MIT License.
 
 ---
 Feel free to contribute and improve this project!
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import readline from "readline"
 import  fs  from "fs"
 import path from "path"
 import { fileURLToPath } from "url"
+import Table from "cli-table"
 
 const rl = readline.createInterface({
     input: stdin,
@@ -15,38 +16,76 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const fileName = "ToDo.txt"
 const filePath = path.join(__dirname, fileName)
+const todoData = () => fs.readFileSync(filePath, 'utf-8')
 
-const handleAddTask = (data) => {
-    data = "\n" + data
-    if(handleShowTask()){
-        fs.appendFileSync(filePath, data, 'utf-8')
+const handleAddTask = (arg) => {
+    if(todoData()){
+        fs.appendFileSync(filePath, `\n${arg}`, 'utf-8')
     }else{
-        fs.writeFileSync(filePath, data, 'utf-8')        
+        fs.writeFileSync(filePath, arg, 'utf-8')        
     }
-    console.log(chalk.greenBright("Successfully added the task"))
+    console.log(chalk.greenBright("\n<=== Successfully added the task ===>"))
     cliOptions()
 }
 
 
+
 const handleShowTask = () => {
-    return fs.readFileSync(filePath, 'utf-8')
+    if(todoData()){
+        const data = todoData().split('\n')
+        const table = new Table({
+            head: ['No', "Tasks"],
+            colWidths: [5, 70]
+        })
+        let temp = data
+        temp.forEach((el,i) => {
+            table.push([i+1,el])
+        })
+        console.log(chalk.whiteBright(table.toString()))
+        cliOptions()
+    }else{
+        console.log(chalk.italic.bgWhite.blackBright("Your todo list is empty :("))
+        cliOptions()
+    }
+}
+
+const handleDelete = (arg) => {
+    let data = todoData().split('\n')
+    if(arg <= data.length && arg){
+        data = data.filter((_, i) => {
+            return i+1 != arg
+        })
+    fs.writeFileSync(filePath, data.join('\n'), 'utf-8')
+    console.log(chalk.greenBright("\n<=== Successfully deleted the task ===>"))
+    }else if(arg){
+        console.log(chalk.bgBlackBright.white("\nInvalid Input. Try again"))
+    }else{
+        console.log(chalk.italic.greenBright("\n<=== Successfully terminated by default ===>"))
+    }
+    cliOptions()
+}
+
+const handleExit = () => {
+    console.log(chalk.redBright("\n<=== Exiting... ===>"))
+    rl.close()
 }
 
 const handleAnswers = (res) => {
     switch (res) {
         case "1":
-            rl.question(chalk.bgGreen.white("Enter the task"), handleAddTask)
+            rl.question(chalk.cyan("Enter the task: ") + " ", handleAddTask)
             break;
         case "2":
-            console.log(chalk.whiteBright(handleShowTask()))
-            cliOptions()
+            handleShowTask()
             break;
         case "3":
-            console.log(chalk.redBright("Exiting..."))
-            rl.close()
+            rl.question(chalk.bgRed.yellowBright("Index of the deleting task? (Default: exit)") + "  ", handleDelete)
+            break;
+        case "4":
+            handleExit()
             break
         default:
-            console.log(chalk.redBright("Invalid Input. Try again"))
+            console.log(chalk.bgBlackBright.white("\nInvalid Input. Try again"))
             cliOptions()
             break;
     }
@@ -56,10 +95,10 @@ const cliOptions = () => {
         chalk.green(
             "\n1. Add Task",
             "\n2. Show saved tasks"
-        ),
-        chalk.red('\n3. Exit')
+        ),chalk.redBright('\n3. Delete a task'),
+        chalk.red('\n4. Exit')
     )
-    rl.question(chalk.yellowBright("Choose an index from above (1,2,3): "), handleAnswers)
+    rl.question(chalk.yellowBright("Choose an index from above (1,2,3,4): "), handleAnswers)
 } 
 
 console.log(chalk.cyanBright(figlet.textSync("Todo CLI", "3D-ASCII")))
